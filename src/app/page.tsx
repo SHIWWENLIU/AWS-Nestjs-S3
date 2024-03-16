4c25024fef31342f50853bf3118540ca8dba7f09
SHIWWENLIU <1033674633@qq.com>
2024-03-16 23:45:17 +1030

    initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="text-center mt-10">
+      <h1>Threads</h1>
+      <p>Threads is a clone of x.com</p>
+    </main>
+  )
+}
